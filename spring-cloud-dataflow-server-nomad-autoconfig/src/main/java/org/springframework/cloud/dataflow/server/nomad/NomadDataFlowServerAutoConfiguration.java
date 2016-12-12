62718ccc19deb4a21da60168bf3057d60079d5a3
Donovan Muller <donovan.muller@gmail.com>
2016-12-13 00:10:13 +0200

    Updated project structure and added documentation.
    * Updated to project structure used in Spring Cloud Server Kubernetes. Closes #4
    * Added reference documentation. Resolves #1

--- spring-cloud-dataflow-server-nomad-autoconfig/src/main/java/org/springframework/cloud/dataflow/server/nomad/NomadDataFlowServerAutoConfiguration.java
+++ spring-cloud-dataflow-server-nomad-autoconfig/src/main/java/org/springframework/cloud/dataflow/server/nomad/NomadDataFlowServerAutoConfiguration.java
@@ -3,15 +3,21 @@ package org.springframework.cloud.dataflow.server.nomad;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.cloud.deployer.resource.docker.DockerResourceLoader;
+import org.springframework.cloud.deployer.resource.maven.MavenProperties;
+import org.springframework.cloud.deployer.resource.maven.MavenResourceLoader;
 import org.springframework.cloud.deployer.resource.support.DelegatingResourceLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.ResourceLoader;
 
 /**
+ * AutoConfiguration for the {@link NomadDataFlowServer}
+ *
  * @author Donovan Muller
  */
 @Configuration
@@ -19,9 +25,11 @@ import org.springframework.core.io.ResourceLoader;
 public class NomadDataFlowServerAutoConfiguration {
 
 	@Bean
-	public DelegatingResourceLoader delegatingResourceLoader() {
+	public DelegatingResourceLoader delegatingResourceLoader(MavenProperties mavenProperties) {
+		MavenResourceLoader mavenLoader = new MavenResourceLoader(mavenProperties);
 		DockerResourceLoader dockerLoader = new DockerResourceLoader();
 		Map<String, ResourceLoader> loaders = new HashMap<>();
+		loaders.put("maven", mavenLoader);
 		loaders.put("docker", dockerLoader);
 		return new DelegatingResourceLoader(loaders);
 	}
---
+++ src/etc/nomad/nexus.nomad
@@ -0,0 +1,55 @@
+job "nexus" {
+	# adjust the region and datacenters accordingly
+	region = "vagrant"
+	datacenters = ["dc1"]
+
+	constraint {
+		attribute = "${attr.kernel.name}"
+		value = "linux"
+	}
+
+	group "nexus" {
+
+		task "nexus" {
+			driver = "docker"
+			config {
+				image = "sonatype/nexus"
+				port_map {
+					http = 8081
+				}
+			}
+
+			service {
+				name = "nexus"
+				# adjust this value (nexus.hashistack.vagrant) to a resolvable host if applicable
+				tags = ["urlprefix-nexus.hashistack.vagrant/"]
+				port = "http"
+				check {
+					name = "Nexus HTTP Check"
+					type = "http"
+					interval = "10s"
+					timeout = "2s"
+					path = "/service/local/status"
+					protocol = "http"
+				}
+			}
+
+			env {
+				CONTEXT_PATH = "/"
+				MIN_HEAP = "256m"
+				MAX_HEAP = "384m"
+			}
+
+			resources {
+				cpu = 500
+				memory = 512
+				network {
+					mbits = 10
+					port "http" {
+						static = 8081
+					}
+				}
+			}
+		}
+	}
+}
